3f2b00c3e4734e5e546a396d5547c6d83e757367
Justin Fagnani <justin@fagnani.com>
2025-11-07 13:00:50 -0800

    [dom-templating-prototype] Add DOMTemplate export

--- packages/dom-templating-prototype/src/index.ts
+++ packages/dom-templating-prototype/src/index.ts
@@ -1,2 +1,23 @@
+import {html, svg, mathml} from './lib/template-result.js';
 export {html, svg, mathml} from './lib/template-result.js';
 export {render} from './lib/render.js';
+
+/**
+ * An namespace object that contains the global DOM Templating API as defined in
+ * the explainer.
+ *
+ * To use this similarly to how the explainer shows, you can do:
+ *
+ * ```ts
+ * import {DOMTemplate} from 'dom-templating-prototype';
+ *
+ * const {html, svg, mathml} = DOMTemplate;
+ *
+ * const template = html`<div>Hello, world!</div>`;
+ * ```
+ */
+export const DOMTemplate = {
+  html,
+  svg,
+  mathml,
+};
