261733b323aeeea97e1f5d5bf394b01eb823b3ac
rictic <rictic@gmail.com>
2025-11-08 15:57:53 -0800

    Initial hacking on a basic component system

---
+++ packages/component-system-prototype/src/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Component system prototype built on the DOM templating API.
+ *
+ * Provides a Preact-like component framework with hooks support.
+ */
+
+// Re-export from dom-templating-prototype for convenience
+export {html, svg, render} from '../../dom-templating-prototype/index.js';
+
+// Export hooks (to be implemented)
+// export {useState} from './hooks/useState.js';
+
+// Export component directive (to be implemented)
+// export {component} from './directives/component.js';
